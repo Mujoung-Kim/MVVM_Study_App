ae7ee77bb57e3612db5e3ff426d3b318671f54aa
Mujoung-Kim <lklkk2002@naver.com>
2020-06-05 10:49:48 +0900

    6/4 Project setup

---
+++ app/src/main/java/company/domain/mvvmstudyapp/ui/auth/LoginActivity.kt
@@ -0,0 +1,15 @@
+package company.domain.mvvmstudyapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import company.domain.mvvmstudyapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+//        <>
+    }
+}
